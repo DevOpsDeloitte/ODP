6934514670418b3fd2d710617306e8d9e616295d
ttimonina <ttimonina@iqsolutions.com>
2014-04-29 15:02:40 -0400

    Manage Teams update

--- SourceCode/ODPTaxonomyWebsite/Evaluation/CreateTeams.aspx
+++
@@ -1,8 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CreateTeams.aspx.cs" Inherits="ODPTaxonomyWebsite.Evaluation.CreateTeams" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-    <h2>
-        Create Teams
-    </h2>
-</asp:Content>
--- SourceCode/ODPTaxonomyWebsite/Evaluation/ManageTeams.aspx.cs
+++ SourceCode/ODPTaxonomyWebsite/Evaluation/ManageTeams.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.Security;
 using System.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text;
 using ODPTaxonomyDAL_TT;
 using ODPTaxonomyUtility_TT;
 
@@ -22,6 +23,7 @@ namespace ODPTaxonomyWebsite.Evaluation
         private string role_odpSup = null;
         private string connString = null;
         private int membersTotal = 3;
+        private Dictionary<int, List<tbl_aspnet_User>> dic_teamUsers = new Dictionary<int, List<tbl_aspnet_User>>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -40,7 +42,7 @@ namespace ODPTaxonomyWebsite.Evaluation
                     lbl_Error.Visible = false;
                     lbl_messageUsers.Text = "";
                     lbl_messageUsers.Visible = false;
-
+                    btn_saveteam.Visible = true;
 
                     //Check current user's role
                     bool isLoggedIn = HttpContext.Current.User.Identity.IsAuthenticated;
@@ -60,6 +62,33 @@ namespace ODPTaxonomyWebsite.Evaluation
         }
 
         #region Methods
+
+        public string GetUserInitials(object firstName, object lastName)
+        {
+            string sFirstName = null;
+            string sLastName = null;
+            string initials = "";
+            StringBuilder sb = new StringBuilder();
+
+            if (firstName != null)
+            {
+                if (lastName != null)
+                {
+                    sFirstName = firstName.ToString();
+                    sLastName = lastName.ToString();
+
+                    if (sFirstName.Length > 0 && sLastName.Length > 0)
+                    {
+                        sb.Append(sFirstName.Substring(0, 1).ToUpper());
+                        sb.Append(sLastName.Substring(0, 1).ToUpper());
+                        
+                        initials = sb.ToString();
+                    }
+                }
+            }
+
+            return initials;
+        }
 
         private void LoadPageData()
         {
@@ -110,7 +139,7 @@ namespace ODPTaxonomyWebsite.Evaluation
                 using (DataDataContext db = new DataDataContext(connString))
                 {
                     var matches = from t in db.tbl_Teams
-                                  join tu in db.tbl_TeamUsers on t.TeamID equals tu.TeamID
+                                  join tu in db.tbl_TeamUsers on t.TeamID equals tu.TeamID                                  
                                   where (t.StatusID == (int)ODPTaxonomyDAL_TT.Status.Active) && (t.TeamTypeID == teamTypeID)
                                   select tu.UserId;
                     list_teamUsers = matches.ToList<System.Guid>();
@@ -119,9 +148,10 @@ namespace ODPTaxonomyWebsite.Evaluation
                 using (DataDataContext db = new DataDataContext(connString))
                 {
                     var matches = from u in db.tbl_aspnet_Users
+                                  join m in db.tbl_aspnet_Memberships on u.UserId equals m.UserId
                                   join ur in db.tbl_aspnet_UsersInRoles on u.UserId equals ur.UserId
                                   join r in db.tbl_aspnet_Roles on ur.RoleId equals r.RoleId
-                                  where roles.Contains(r.RoleName) && !list_teamUsers.Contains(u.UserId)
+                                  where roles.Contains(r.RoleName) && !list_teamUsers.Contains(u.UserId) && m.IsApproved
                                   select u;
                     list_users = matches.ToList<tbl_aspnet_User>();
                 }
@@ -134,8 +164,9 @@ namespace ODPTaxonomyWebsite.Evaluation
                 }
                 else
                 {
-                    lbl_messageUsers.Text = "No users are currently evailable for you to select.";
+                    lbl_messageUsers.Text = "No users are currently evailable for you to select for a new team.";
                     lbl_messageUsers.Visible = true;
+                    btn_saveteam.Visible = false;
                 }
             }
 
@@ -144,16 +175,53 @@ namespace ODPTaxonomyWebsite.Evaluation
         protected void LoadTeams(int teamTypeID)
         {
             List<tbl_Team> list_teams = new List<tbl_Team>();
+            
+            int currentTeamID = 0;
+            tbl_aspnet_User currentUser = null;
+            List<tbl_aspnet_User> currentUserList = null;
+
             //Check current user's role
             if (!String.IsNullOrEmpty(connString))
             {
                 using (DataDataContext db = new DataDataContext(connString))
                 {
                     var matches = from t in db.tbl_Teams
-                                  join tu in db.tbl_TeamUsers on t.TeamID equals tu.TeamID
                                   where (t.StatusID == (int)ODPTaxonomyDAL_TT.Status.Active) && (t.TeamTypeID == teamTypeID)
                                   select t;
                     list_teams = matches.ToList<tbl_Team>();
+
+                    var matches_2 = from t in db.tbl_Teams
+                                    join tu in db.tbl_TeamUsers on t.TeamID equals tu.TeamID
+                                    join u in db.tbl_aspnet_Users on tu.UserId equals u.UserId
+                                    where (t.StatusID == (int)ODPTaxonomyDAL_TT.Status.Active) && (t.TeamTypeID == teamTypeID)
+                                    select new { t.TeamID, u.UserId, u.UserName, u.UserLastName, u.UserFirstName};
+
+                    foreach (var i in matches_2)
+                    {
+                        currentTeamID = i.TeamID;
+                        currentUser = new tbl_aspnet_User();
+                        currentUser.UserId = i.UserId;
+                        currentUser.UserName = i.UserName;
+                        currentUser.UserLastName = i.UserLastName;
+                        currentUser.UserFirstName = i.UserFirstName;
+
+                        if (!dic_teamUsers.ContainsKey(currentTeamID))
+                        {
+                            currentUserList = new List<tbl_aspnet_User>();
+                            currentUserList.Add(currentUser);
+
+                            dic_teamUsers.Add(currentTeamID, currentUserList);
+                        }
+                        else
+                        {
+                            currentUserList = dic_teamUsers[currentTeamID];
+                            if (!currentUserList.Contains(currentUser))
+                            {
+                                currentUserList.Add(currentUser);
+                            }
+                        }
+                    }
+
                 }
 
                 if (list_teams.Count > 0)
@@ -161,6 +229,8 @@ namespace ODPTaxonomyWebsite.Evaluation
                     rpt_teams.DataSource = list_teams;
                     rpt_teams.DataBind();
                 }
+
+                
             }
         }
 
@@ -168,8 +238,26 @@ namespace ODPTaxonomyWebsite.Evaluation
 
         #region EventHandlers
 
-        protected void rp_topics_OnItemDataBound(object sender, RepeaterItemEventArgs e)
+        protected void rp_teams_OnItemDataBound(object sender, RepeaterItemEventArgs e)
         {
+            HiddenField hf_teamID = e.Item.FindControl("hf_teamID") as HiddenField;
+            Repeater rpt_teamMembers = e.Item.FindControl("rpt_teamMembers") as Repeater;
+            int teamID = 0;
+            List<tbl_aspnet_User> currentUserList = null;
+
+            if (hf_teamID != null)
+            {
+                if (Int32.TryParse(hf_teamID.Value.ToString(), out teamID))
+                {
+                    currentUserList = dic_teamUsers[teamID];
+                    if (rpt_teamMembers != null)
+                    {
+                        rpt_teamMembers.DataSource = currentUserList;
+                        rpt_teamMembers.DataBind();
+                    }
+                }
+            }
+
         }
 
         protected void btn_saveteam_Click(object sender, EventArgs e)
@@ -183,64 +271,105 @@ namespace ODPTaxonomyWebsite.Evaluation
 
                 //Validate User's Input   
                 HiddenField hf_userID = null;
+                HiddenField hf_userInitials = null;
                 int teamtypeId;
                 bool teamtypeIsOK = Int32.TryParse(hf_teamTypeId.Value.ToString(), out teamtypeId);
                 CheckBox chBox = null;
                 int membersCheckedCount = 0;
                 System.Guid userId = Guid.Empty;
-                List<Guid> list_userIds = new List<Guid>();
+                StringBuilder sb = new StringBuilder();
+                string initials = "";
+                List<tbl_aspnet_User> listUsers = new List<tbl_aspnet_User>();
+                tbl_aspnet_User currentUser = null;
+                List<System.Guid> list_teamUsersID = new List<System.Guid>();
 
-                foreach (RepeaterItem ri in rpt_users.Items)
+                //get list of users already in teams
+                if (!String.IsNullOrEmpty(connString) && teamtypeIsOK)
                 {
-                    hf_userID = ri.FindControl("hf_userID") as HiddenField;
-                    chBox = ri.FindControl("checkbox") as CheckBox;
-
-                    if (chBox != null)
+                    //Select users available for this team type
+                    using (DataDataContext db = new DataDataContext(connString))
                     {
-                        if (hf_userID != null)
-                        {
-                            
-                            if (chBox.Checked)
-                            {
-                                userId = Guid.Parse(hf_userID.Value);
-                                list_userIds.Add(userId);
-                                membersCheckedCount++;
-                            }
-                        }
+                        var matches = from t in db.tbl_Teams
+                                      join tu in db.tbl_TeamUsers on t.TeamID equals tu.TeamID
+                                      where (t.StatusID == (int)ODPTaxonomyDAL_TT.Status.Active) && (t.TeamTypeID == teamtypeId)
+                                      select tu.UserId;
+                        list_teamUsersID = matches.ToList<System.Guid>();
                     }
 
+                    //create a list of selected users
+                    foreach (RepeaterItem ri in rpt_users.Items)
+                    {
+                        hf_userID = ri.FindControl("hf_userID") as HiddenField;
+                        hf_userInitials = ri.FindControl("hf_userInitials") as HiddenField;
+                        chBox = ri.FindControl("checkbox") as CheckBox;
+
+                        if (chBox != null)
+                        {
+                            if (hf_userID != null && hf_userInitials != null)
+                            {
+
+                                if (chBox.Checked)
+                                {
+                                    userId = Guid.Parse(hf_userID.Value);
+                                    initials = hf_userInitials.Value;
+
+                                    //verify that user is NOT in a team already
+                                    if (!list_teamUsersID.Contains(userId))
+                                    {
+                                        currentUser = new tbl_aspnet_User();
+                                        currentUser.UserId = userId;
+                                        currentUser.UserFirstName = "";
+                                        currentUser.UserLastName = "";
+                                        currentUser.UserName = initials;
+                                        listUsers.Add(currentUser);
+                                        membersCheckedCount++;
+                                    }
+                                    else
+                                    {
+                                        lbl_messageUsers.Text = "The user " + initials + " is in a team already.";
+                                        lbl_messageUsers.Visible = true;
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+
+                    }     
+
+
+
                 }
+
 
                 if (membersCheckedCount == membersTotal)
                 {
                     //Save new Team
-                    if (teamtypeIsOK)
+                    using (DataDataContext db = new DataDataContext(connString))
                     {
-                        if (!String.IsNullOrEmpty(connString))
+                        tbl_Team team = new tbl_Team();
+                        foreach (tbl_aspnet_User u in listUsers)
                         {
-                            using (DataDataContext db = new DataDataContext(connString))
-                            {
-                                foreach (Guid uId in list_userIds)
-                                {
-                                    tbl_Team team = new tbl_Team();
-                                    tbl_TeamUser teamUser = new tbl_TeamUser();
-                                    teamUser.UserId = uId;
-                                    team.tbl_TeamUsers.Add(teamUser);
-                                    team.StatusID = (int)ODPTaxonomyDAL_TT.Status.Active;
-                                    team.TeamTypeID = teamtypeId;
-                                    team.Createdby = userCurrentId;
-                                    team.CreatedDateTime = DateTime.Now;
-
-
-                                    db.tbl_Teams.InsertOnSubmit(team);
-                                    db.SubmitChanges();
-                                }
-                            }
+                            tbl_TeamUser teamUser = new tbl_TeamUser();
+                            teamUser.UserId = u.UserId;
+                            team.tbl_TeamUsers.Add(teamUser);
+                            sb.Append(u.UserName + "_");
                         }
+                        //sb.Length--;
+                        initials = sb.ToString();
+
+                        team.StatusID = (int)ODPTaxonomyDAL_TT.Status.Active;
+                        team.TeamTypeID = teamtypeId;
+                        team.Createdby = userCurrentId;
+                        team.CreatedDateTime = DateTime.Now;
+                        team.TeamCode = Common.GetTeamCode(initials);
+
+                        db.tbl_Teams.InsertOnSubmit(team);
+                        db.SubmitChanges();
                     }
 
                     //Reload Page Data
-                    LoadPageData();
+                    //LoadPageData();
+                   Response.Redirect("ManageTeams.aspx", true);
                 }
                 else
                 {
