d721e5dc90a30ea06105ea11b1a3798c5ef42d0d
jazchan <jchan@iqsolutions.com>
2014-05-02 15:09:28 -0400

    updates for Account Management

--- SourceCode/ODPTaxonomyWebsite/AccountManagement/ViewUsers.aspx
+++ SourceCode/ODPTaxonomyWebsite/AccountManagement/ViewUsers.aspx
@@ -5,4 +5,23 @@
     <h2>
         User List
     </h2>
+
+    <asp:Label ID="lbl_error_message" CssClass="errorMessage" runat="server" Visible="false" />
+
+    <asp:GridView ID="gvw_users" runat="server" AutoGenerateColumns="false" 
+        AllowSorting="true" OnSorting="gvw_users_OnSorting" >
+        <Columns>
+            <asp:BoundField HeaderText="UserID" DataField="UserID" Visible="false" />
+            <asp:BoundField HeaderText="User ID" DataField="UserName" SortExpression="UserName" />
+            <asp:BoundField HeaderText="First Name" DataField="UserFirstName" SortExpression="UserFirstName" />
+            <asp:BoundField HeaderText="Last Name" DataField="UserLastName" SortExpression="UserLastName" />
+            <asp:BoundField HeaderText="Email" DataField="Email"/>
+            <asp:TemplateField HeaderText="Active">
+                <ItemTemplate><%# (Boolean.Parse(Eval("IsApproved").ToString())) ? "Yes" : "No" %></ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Roles" >
+                <ItemTemplate><%# GetUserRoles(Eval("UserName").ToString()) %></ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
 </asp:Content>
--- SourceCode/ODPTaxonomyWebsite/AccountManagement/ViewUsers.aspx.cs
+++ SourceCode/ODPTaxonomyWebsite/AccountManagement/ViewUsers.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Linq.Expressions;
+using ODPTaxonomyAccountDAL;
+using ODPTaxonomyUtility_TT;
 
 namespace ODPTaxonomyWebsite.AccountManagement
 {
@@ -11,7 +15,96 @@ namespace ODPTaxonomyWebsite.AccountManagement
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    BindData(null, null);
 
+                }
+                catch (Exception ex)
+                {
+                    Utils.LogError(ex);
+                }
+            }
+        }
+
+        
+
+        protected void BindData(string sortBy, string sortDirection)
+        {
+            using (AccountDataLinqDataContext db = new AccountDataLinqDataContext(AccountDAL.connString))
+            {
+                gvw_users.DataSource = db.select_users(sortBy, sortDirection);
+                gvw_users.DataBind();
+            }
+        }
+
+        protected SortDirection GetSortDirection(string column)
+        {
+            SortDirection l_sortDirection = SortDirection.Ascending;
+
+            if (Session["AM_SortExpression"] != null)
+            {
+                string l_prevSortExpression = Session["AM_SortExpression"].ToString();
+
+                // check if same column
+                if (l_prevSortExpression == column)
+                {
+                    string l_prevSortDirection = Session["AM_SortDirection"].ToString();
+                    if ((l_prevSortDirection != null) && (l_prevSortDirection.ToUpper() == "ASCENDING")){
+                        l_sortDirection = SortDirection.Descending;
+                    }
+                }
+            }
+
+            Session["AM_SortDirection"] = l_sortDirection.ToString();
+            Session["AM_SortExpression"] = column;
+
+            return l_sortDirection;
+        }
+        
+        protected string GetUserRoles(string userName)
+        {
+            string l_roleList = "";
+            foreach (string role in Roles.GetRolesForUser(userName))
+            {
+                l_roleList += role + "|";
+            }
+
+            return l_roleList.TrimEnd('|').Replace("|", "<br />").ToString();
+            
+        }
+
+
+        protected void gvw_users_OnSorting(object sender, GridViewSortEventArgs e)
+        {
+            string l_sortExpression = e.SortExpression;
+            string l_sortDirection;
+
+            if (GetSortDirection(l_sortExpression) == SortDirection.Ascending)
+            {
+                l_sortDirection = "ASC";
+            }
+            else
+            {
+                l_sortDirection = "DESC";
+            }
+
+            BindData(l_sortExpression, l_sortDirection);
+        }
+
+        
+
+        //protected void gvw_users_OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        //{
+
+        //}
+
+        protected void HandlePageError(string message)
+        {
+            lbl_error_message.Visible = true;
+            lbl_error_message.Text = message;
         }
     }
 }
