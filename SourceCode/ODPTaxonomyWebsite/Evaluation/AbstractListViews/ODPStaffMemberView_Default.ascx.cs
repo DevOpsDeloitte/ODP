2a67c6c56f05243b63c833a03b97f4791509f8b8
JYE <jye@iqsolutions.com>
2014-05-21 15:26:41 -0400

    abstract list update

--- SourceCode/ODPTaxonomyWebsite/Evaluation/AbstractListViews/ODPStaffMemberView_Default.ascx.cs
+++ SourceCode/ODPTaxonomyWebsite/Evaluation/AbstractListViews/ODPStaffMemberView_Default.ascx.cs
@@ -78,6 +78,12 @@ namespace ODPTaxonomyWebsite.Evaluation.AbstractListViews
                            IsParent = true
                        };
 
+            if (AbstractViewGridView.Attributes["CurrentSortExp"] != null)
+            {
+                sort = AbstractViewGridView.Attributes["CurrentSortExp"];
+                direction = AbstractViewGridView.Attributes["CurrentSortDir"] == "ASC" ? SortDirection.Ascending : SortDirection.Descending;
+            }
+
             switch (sort)
             {
                 case "ApplicationID":
