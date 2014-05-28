bb6a82bec3d4f214db980d492a9fe07e5d9c43c8
ttimonina <ttimonina@iqsolutions.com>
2014-05-28 15:04:51 -0400

    multiple conflicts revolved. Deployed to DEV server on May 28 at 3 pm

--- SourceCode/ODPTaxonomyWebsite/Evaluation/Evaluation.aspx.cs
+++ SourceCode/ODPTaxonomyWebsite/Evaluation/Evaluation.aspx.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using ODPTaxonomyUtility_TT;
-using ODPTaxonomyCommon;
 
 namespace ODPTaxonomyWebsite.Evaluation
 {
@@ -13,32 +11,7 @@ namespace ODPTaxonomyWebsite.Evaluation
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            try
-            {
-                if (!Page.IsPostBack)
-                {
-                    lbl_messageUsers.Visible = false;
 
-                    if (Session["ViewAbstractToEvaluation"] != null)
-                    {
-                        ViewAbstractToEvaluation values = (ViewAbstractToEvaluation)Session["ViewAbstractToEvaluation"];
-                        lbl_messageUsers.Visible = true;
-                        lbl_messageUsers.Text = "User ID: " + values.UserId.ToString() + "; EvaluationId = " + values.EvaluationId.ToString() 
-                            + "; SubmissionTypeId = " + values.SubmissionTypeId.ToString() + "; Mode: " + values.ViewMode.ToString();
-                        Session.Remove("ViewAbstractToEvaluation");
-                    }
-                    else
-                    {
-                        lbl_messageUsers.Visible = true;
-                        lbl_messageUsers.Text = "No Session";
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Utils.LogError(ex);
-                throw new Exception("An error has occured while loading page data.");
-            }
         }
     }
 }
--- SourceCode/ODPTaxonomyWebsite/Evaluation/ViewAbstractList.aspx.cs
+++ SourceCode/ODPTaxonomyWebsite/Evaluation/ViewAbstractList.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using ODPTaxonomyDAL_TT;
+using ODPTaxonomyWebsite.Evaluation.AbstractListViews;
 
 namespace ODPTaxonomyWebsite.Evaluation
 {
@@ -11,7 +14,9 @@ namespace ODPTaxonomyWebsite.Evaluation
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["CurrentRole"] != null)
+            List<string> roles = Roles.GetRolesForUser().ToList();
+            roles.Remove("Coder");
+
             string Mainview = null;
             int MainviewIndex = -1;
             string Subview = null;
@@ -19,6 +24,10 @@ namespace ODPTaxonomyWebsite.Evaluation
 
             SetPager();
 
+            if (!IsPostBack)
+            {
+                if (roles.Count == 1)
+                {
                     Mainview = roles[0];
                 }
             }
@@ -30,10 +39,12 @@ namespace ODPTaxonomyWebsite.Evaluation
 
                     Subview = SubviewDDL.SelectedValue;
                     SubviewIndex = SubviewDDL.SelectedIndex;
+                }
                 else
                 {
                     Mainview = MainviewDDL.SelectedValue;
                     MainviewIndex = MainviewDDL.SelectedIndex;
+
                     Subview = SubviewDDL.SelectedValue;
                     SubviewIndex = SubviewDDL.SelectedIndex;
 
@@ -61,6 +72,7 @@ namespace ODPTaxonomyWebsite.Evaluation
             if (HttpContext.Current.Request.Cookies["Pager"] != null)
             {
                 int TempPagerSize;
+
                 if (int.TryParse(HttpContext.Current.Request.Cookies["Pager"]["Size"].ToString(), out TempPagerSize))
                 {
                     switch (TempPagerSize)
@@ -80,13 +92,94 @@ namespace ODPTaxonomyWebsite.Evaluation
                 }
             }
         }
+
+        /**
+         * Loads data for select a view dropdown
+         */
         protected void LoadViewDropDownData(List<string> roles, string SelectedView)
+        {
+            if (roles.Count > 1)
+            {
+                MainviewDDL.Items.Clear();
                 MainviewDDL.Items.Add(new ListItem("Select a View", ""));
+                foreach (string role in roles)
+                {
                     ListItem item = new ListItem(role + " View", role);
                     item.Selected = SelectedView == role;
 
                     MainviewDDL.Items.Add(item);
+                }
+                MainviewDDL.Visible = true;
+            }
+            else
+            {
+                MainviewDDL.Visible = false;
+            }
+        }
+
+        protected void LoadSubviewDropDownData(string role, int selectedIndex = -1)
+        {
+            SubviewDDL.Items.Clear();
+            switch (role)
+            {
+                case "CoderSupervisor":
+                    SubviewLabel.Text = "Abstract Types:";
+                    SubviewLabel.Visible = true;
+
+                    SubviewDDL.Items.Add(new ListItem("Coded Abstracts", "coded"));
+                    SubviewDDL.Items.Add(new ListItem("Open Abstracts", "open"));
+
+                    if (selectedIndex > -1)
+                    {
+                        SubviewDDL.SelectedIndex = selectedIndex;
+                    }
+
+                    SubviewDDL.Visible = true;
+                    SubviewPanel.Visible = true;
+                    break;
+                case "ODPStaffMember":
+                    SubviewLabel.Text = "Abstract Types:";
+                    SubviewLabel.Visible = true;
+
+                    SubviewDDL.Items.Add(new ListItem("Default View", ""));
+                    SubviewDDL.Items.Add(new ListItem("In Review List", "review"));
+                    SubviewDDL.Items.Add(new ListItem("In Review List - Uncoded Only", "uncoded"));
+
+                    if (selectedIndex > -1)
+                    {
+                        SubviewDDL.SelectedIndex = selectedIndex;
+                    }
+
+                    SubviewDDL.Visible = true;
+                    SubviewPanel.Visible = true;
+                    break;
+                case "ODPStaffSupervisor":
+                    SubviewLabel.Text = "Abstract Types:";
+                    SubviewLabel.Visible = true;
+
+                    SubviewDDL.Items.Add(new ListItem("Default View", ""));
+                    SubviewDDL.Items.Add(new ListItem("Open Abstract", "open"));
+
+                    if (selectedIndex > -1)
+                    {
+                        SubviewDDL.SelectedIndex = selectedIndex;
+                    }
+
+                    SubviewDDL.Visible = true;
+                    SubviewPanel.Visible = true;
+                    break;
+
+                case "Admin":
+                default:
+                    SubviewLabel.Visible = false;
+                    SubviewDDL.Visible = false;
+                    SubviewPanel.Visible = false;
+                    break;
+            }
+        }
+
         protected void RenderAbstractListView(string Mainview, string Subview = "")
+        {
             AdminView.Visible = false;
             CoderSupervisor_Coded.Visible = false;
             CoderSupervisor_Open.Visible = false;
@@ -95,22 +188,71 @@ namespace ODPTaxonomyWebsite.Evaluation
             ODPStaffView_Review_Uncoded.Visible = false;
             ODPSupervisorView_Default.Visible = false;
             ODPSupervisorView_Open.Visible = false;
+
             switch (Mainview)
-                    AdminView.Visible = true;
-                    if (Subview == "open")
-                        CoderSupervisor_Open.Visible = true;
-                    else
-                        CoderSupervisor_Coded.Visible = true;
-                    if (Subview == "open")
-                        ODPSupervisorView_Open.Visible = true;
-                        ODPSupervisorView_Default.Visible = true;
-                    if (Subview == "review")
-                        ODPStaffView_Review.Visible = true;
-                    else if (Subview == "uncoded")
-                        ODPStaffView_Review_Uncoded.Visible = true;
-                        ODPStaffView_Default.Visible = true;
             {
-                lbl_messageUsers.Text = "Your Current Role: " + Session["CurrentRole"].ToString();
+                case "Admin":
+                    AdminView.Visible = true;
+                    break;
+                case "CoderSupervisor":
+                    if (Subview == "open")
+                    {
+                        CoderSupervisor_Open.Visible = true;
+                    }
+                    else
+                    {
+                        CoderSupervisor_Coded.Visible = true;
+                    }
+                    break;
+                case "ODPStaffSupervisor":
+                    if (Subview == "open")
+                    {
+                        ODPSupervisorView_Open.Visible = true;
+                    }
+                    else
+                    {
+                        ODPSupervisorView_Default.Visible = true;
+                    }
+                    break;
+                case "ODPStaffMember":
+                    if (Subview == "review")
+                    {
+                        ODPStaffView_Review.Visible = true;
+                    }
+                    else if (Subview == "uncoded")
+                    {
+                        ODPStaffView_Review_Uncoded.Visible = true;
+                    }
+                    else
+                    {
+                        ODPStaffView_Default.Visible = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected void PagerSizeChangeHandler(object sender, EventArgs e)
+        {
+            int NewPagerSize;
+
+            if (int.TryParse(PagerSizeDDL.SelectedValue, out NewPagerSize))
+            {
+                switch (NewPagerSize)
+                {
+                    case 25:
+                    case 50:
+                    case 100:
+                        HttpCookie PagerCookie = new HttpCookie("Pager");
+                        PagerCookie["size"] = NewPagerSize.ToString();
+                        PagerCookie.Expires = DateTime.Now.AddYears(1);
+
+                        Response.Cookies.Add(PagerCookie);
+                        break;
+                    default:
+                        break;
+                }
             }
 
             Response.Redirect("~/Evaluation/ViewAbstractList.aspx");
