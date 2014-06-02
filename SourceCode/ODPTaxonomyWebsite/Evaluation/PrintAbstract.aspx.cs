45d23a9f614ee7d8ec0b554e9e872402d451641d
Sunil Taneja <suniltaneja@me.com>
2014-06-02 11:19:23 -0400

    adding new files for print page.

---
+++ SourceCode/ODPTaxonomyWebsite/Evaluation/PrintAbstract.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ODPTaxonomyDAL_ST;
+using ODPTaxonomyWebsite.Evaluation.Classes;
+
+namespace ODPTaxonomyWebsite.Evaluation
+{
+    public partial class PrintAbstract : System.Web.UI.Page
+    {
+        public int abstractId = 0;
+        public string userID = "";
+        public string projectTitle;
+        public string administeringIC;
+        public string applicationID;
+        public string PIProjectLeader;
+        public string FY;
+        public string ProjectNumber;
+        public string desc;
+        public string healthpart;
+
+
+        private DataDataContext db = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!String.IsNullOrEmpty(Request.QueryString["id"]))
+            {
+                abstractId = int.Parse(Request.QueryString["id"]);
+            }
+            if (abstractId > 0 && HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                db = DBData.GetDataContext();
+               
+                MembershipUser user = Membership.GetUser();
+                userID = user.UserName;
+
+                var absrec = db.Abstracts.Where(a => a.AbstractID == abstractId).FirstOrDefault();
+                if (absrec != null)
+                {
+                    projectTitle = absrec.ProjectTitle;
+                    administeringIC = absrec.AdministeringIC;
+                    applicationID = absrec.ApplicationID.ToString();
+                    PIProjectLeader = absrec.PIProjectLeader;
+                    FY = absrec.FY;
+                    ProjectNumber = absrec.ProjectNumber;
+                    desc = absrec.AbstractDescPart;
+                    healthpart = absrec.AbstractPublicHeathPart;
+
+                }
+
+                
+            }
+
+        }
+    }
+}
