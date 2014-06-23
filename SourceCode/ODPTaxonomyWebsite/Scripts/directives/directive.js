fb32f435ac0ea796679121b002c48b792a16e1eb
Sunil Taneja <suniltaneja@me.com>
2014-06-23 09:51:37 -0400

    Update for new requirements. Show Coder/ODP instead of teams.

--- SourceCode/ODPTaxonomyWebsite/Scripts/directives/directive.js
+++ SourceCode/ODPTaxonomyWebsite/Scripts/directives/directive.js
@@ -144,6 +144,8 @@ app.directive("outcomeBox", function ($rootScope) {
                 }
 
                 //console.log( x + '       ' + retval);
+                if (retval[0] != "") retval[0] = "ODP";
+                if (retval[1] != "") retval[1] = "CODER";
 
 
                 return retval;
