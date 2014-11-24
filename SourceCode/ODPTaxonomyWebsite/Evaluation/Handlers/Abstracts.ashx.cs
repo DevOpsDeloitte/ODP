d807da57936233bf8a1bd960e023bd205c3db847
Sunil Taneja <suniltaneja@me.com>
2014-11-24 10:29:30 -0500

    updates for abstract list.

--- SourceCode/ODPTaxonomyWebsite/Evaluation/Handlers/Abstracts.ashx.cs
+++ SourceCode/ODPTaxonomyWebsite/Evaluation/Handlers/Abstracts.ashx.cs
@@ -43,19 +43,19 @@ namespace ODPTaxonomyWebsite.Evaluation.Handlers
 
                     switch (filter)
                     {
-                        case "open":
-                            parentAbstracts = this.GetParentAbstractsODPSupervisorOpen();   
-                            ALR = AbstractListViewHelper.ProcessAbstracts2(parentAbstracts, AbstractViewRole.ODPSupervisor);
-                            serializeResponse(context, ALR);
+                        //case "open":
+                        //    parentAbstracts = this.GetParentAbstractsODPSupervisorOpen();   
+                        //    ALR = AbstractListViewHelper.ProcessAbstracts2(parentAbstracts, AbstractViewRole.ODPSupervisor);
+                        //    serializeResponse(context, ALR);
                             
-                            break;
+                        //    break;
 
-                        case "uncoded":
-                            parentAbstracts = this.GetParentAbstractsODPSupervisorReviewUnCoded();  
-                            ALR = AbstractListViewHelper.ProcessAbstracts2(parentAbstracts, AbstractViewRole.ODPSupervisor);
-                            serializeResponse(context, ALR);
+                        //case "uncoded":
+                        //    parentAbstracts = this.GetParentAbstractsODPSupervisorReviewUnCoded();  
+                        //    ALR = AbstractListViewHelper.ProcessAbstracts2(parentAbstracts, AbstractViewRole.ODPSupervisor);
+                        //    serializeResponse(context, ALR);
 
-                            break;
+                        //    break;
 
                         case "review" :
                             parentAbstracts = this.GetParentAbstractsODPSupervisorReview();
@@ -376,58 +376,6 @@ namespace ODPTaxonomyWebsite.Evaluation.Handlers
 
             return AbstractListViewHelper.SortAbstracts(finalabstracts, sort, direction);
         }
-
-        protected List<AbstractListRow> GetParentAbstractsODPSupervisorDefault(string sort = "", SortDirection direction = SortDirection.Ascending)
-        {
-            string connStr = ConfigurationManager.ConnectionStrings["ODPTaxonomy"].ConnectionString;
-            DataJYDataContext db = new DataJYDataContext(connStr);
-
-            //System.Diagnostics.Stopwatch objStopWatch = new System.Diagnostics.Stopwatch();
-            //objStopWatch.Start();
-
-            var data = from a in db.Abstracts
-                       join h in db.AbstractStatusChangeHistories on a.AbstractID equals h.AbstractID
-                       join s in db.AbstractStatus on h.AbstractStatusID equals s.AbstractStatusID
-                       where (
-                          h.AbstractStatusID >= (int)AbstractStatusEnum.CONSENSUS_COMPLETE_WITH_NOTES_1N &&
-                          h.AbstractStatusChangeHistoryID == db.AbstractStatusChangeHistories
-                           .Where(h2 => h2.AbstractID == a.AbstractID)
-                           .Select(h2 => h2.AbstractStatusChangeHistoryID).Max()
-                           )
-                       select new AbstractListRow
-                       {
-                           AbstractID = a.AbstractID,
-                           ProjectTitle = a.ProjectTitle + " (" + s.AbstractStatusCode + ")",
-                           ApplicationID = a.ApplicationID,
-                           AbstractStatusID = s.AbstractStatusID,
-                           AbstractStatusCode = s.AbstractStatusCode,
-                           StatusDate = h.CreatedDate,
-                           EvaluationID = h.EvaluationId,
-                           IsParent = true
-                       };
-
-            //var genSQL = db.GetCommand(data).CommandText;
-            //System.Diagnostics.Trace.WriteLine(genSQL);
-
-           
-
-            List<AbstractListRow> abstracts = data.ToList();
-
-            //objStopWatch.Stop();
-            //System.Diagnostics.Trace.Write("The time taken to execute query without compilation is : " +
-            //objStopWatch.ElapsedMilliseconds.ToString() + " MillionSeconds<br>");
-            //objStopWatch.Reset();
-           
-            //db.Log = Console.Out;
-
-            //if (AbstractViewGridView.Attributes["CurrentSortExp"] != null)
-            //{
-            //    sort = AbstractViewGridView.Attributes["CurrentSortExp"];
-            //    direction = AbstractViewGridView.Attributes["CurrentSortDir"] == "ASC" ? SortDirection.Ascending : SortDirection.Descending;
-            //}
-
-            return AbstractListViewHelper.SortAbstracts(abstracts, sort, direction);
-        }
         protected List<AbstractListRow> GetParentAbstractsODPSupervisorReview(string sort = "", SortDirection direction = SortDirection.Ascending)
         {
             string connString = ConfigurationManager.ConnectionStrings["ODPTaxonomy"].ConnectionString;
@@ -466,82 +414,135 @@ namespace ODPTaxonomyWebsite.Evaluation.Handlers
 
             return AbstractListViewHelper.SortAbstracts(abstracts, sort, direction);
         }
-        protected List<AbstractListRow> GetParentAbstractsODPSupervisorOpen(string sort = "", SortDirection direction = SortDirection.Ascending)
-        {
-            string connStr = ConfigurationManager.ConnectionStrings["ODPTaxonomy"].ConnectionString;
-            DataJYDataContext db = new DataJYDataContext(connStr);
 
-            var data = from a in db.Abstracts
-                       join h in db.AbstractStatusChangeHistories on a.AbstractID equals h.AbstractID
-                       join s in db.AbstractStatus on h.AbstractStatusID equals s.AbstractStatusID
-                       where (
-                          (h.AbstractStatusID == (int)AbstractStatusEnum.RETRIEVED_FOR_ODP_CODING_2 ||
-                          h.AbstractStatusID == (int)AbstractStatusEnum.CODED_BY_ODP_STAFF_2A) &&
-                          h.AbstractStatusChangeHistoryID == db.AbstractStatusChangeHistories
-                           .Where(h2 => h2.AbstractID == a.AbstractID)
-                           .Select(h2 => h2.AbstractStatusChangeHistoryID).Max()
-                           )
-                       select new AbstractListRow
-                       {
-                           AbstractID = a.AbstractID,
-                           ProjectTitle = a.ProjectTitle + " (" + s.AbstractStatusCode + ")",
-                           ApplicationID = a.ApplicationID,
-                           AbstractStatusID = s.AbstractStatusID,
-                           AbstractStatusCode = s.AbstractStatusCode,
-                           StatusDate = h.CreatedDate,
-                           EvaluationID = h.EvaluationId,
-                           KappaType = KappaTypeEnum.K1,
-                           IsParent = true
-                       };
+        //protected List<AbstractListRow> GetParentAbstractsODPSupervisorDefault(string sort = "", SortDirection direction = SortDirection.Ascending)
+        //{
+        //    string connStr = ConfigurationManager.ConnectionStrings["ODPTaxonomy"].ConnectionString;
+        //    DataJYDataContext db = new DataJYDataContext(connStr);
 
-            List<AbstractListRow> abstracts = data.ToList();
+        //    //System.Diagnostics.Stopwatch objStopWatch = new System.Diagnostics.Stopwatch();
+        //    //objStopWatch.Start();
 
-            //if (AbstractViewGridView.Attributes["CurrentSortExp"] != null)
-            //{
-            //    sort = AbstractViewGridView.Attributes["CurrentSortExp"];
-            //    direction = AbstractViewGridView.Attributes["CurrentSortDir"] == "ASC" ? SortDirection.Ascending : SortDirection.Descending;
-            //}
+        //    var data = from a in db.Abstracts
+        //               join h in db.AbstractStatusChangeHistories on a.AbstractID equals h.AbstractID
+        //               join s in db.AbstractStatus on h.AbstractStatusID equals s.AbstractStatusID
+        //               where (
+        //                  h.AbstractStatusID >= (int)AbstractStatusEnum.CONSENSUS_COMPLETE_WITH_NOTES_1N &&
+        //                  h.AbstractStatusChangeHistoryID == db.AbstractStatusChangeHistories
+        //                   .Where(h2 => h2.AbstractID == a.AbstractID)
+        //                   .Select(h2 => h2.AbstractStatusChangeHistoryID).Max()
+        //                   )
+        //               select new AbstractListRow
+        //               {
+        //                   AbstractID = a.AbstractID,
+        //                   ProjectTitle = a.ProjectTitle + " (" + s.AbstractStatusCode + ")",
+        //                   ApplicationID = a.ApplicationID,
+        //                   AbstractStatusID = s.AbstractStatusID,
+        //                   AbstractStatusCode = s.AbstractStatusCode,
+        //                   StatusDate = h.CreatedDate,
+        //                   EvaluationID = h.EvaluationId,
+        //                   IsParent = true
+        //               };
 
-            return AbstractListViewHelper.SortAbstracts(abstracts, sort, direction);
-        }
-        protected List<AbstractListRow> GetParentAbstractsODPSupervisorReviewUnCoded(string sort = "", SortDirection direction = SortDirection.Ascending)
-        {
-            string connStr = ConfigurationManager.ConnectionStrings["ODPTaxonomy"].ConnectionString;
-            DataJYDataContext db = new DataJYDataContext(connStr);
+        //    //var genSQL = db.GetCommand(data).CommandText;
+        //    //System.Diagnostics.Trace.WriteLine(genSQL);
 
-            var data = from a in db.Abstracts
-                       join h in db.AbstractStatusChangeHistories on a.AbstractID equals h.AbstractID
-                       join s in db.AbstractStatus on h.AbstractStatusID equals s.AbstractStatusID
-                       join rv in db.AbstractReviewLists on a.AbstractID equals rv.AbstractID
-                       where (
-                          h.AbstractStatusID == (int)AbstractStatusEnum.CONSENSUS_COMPLETE_WITH_NOTES_1N &&
-                          h.AbstractStatusChangeHistoryID == db.AbstractStatusChangeHistories
-                           .Where(h2 => h2.AbstractID == a.AbstractID)
-                           .Select(h2 => h2.AbstractStatusChangeHistoryID).Max()
-                           )
-                       select new AbstractListRow
-                       {
-                           AbstractID = a.AbstractID,
-                           ProjectTitle = a.ProjectTitle + " (" + s.AbstractStatusCode + ")",
-                           ApplicationID = a.ApplicationID,
-                           AbstractStatusID = s.AbstractStatusID,
-                           AbstractStatusCode = s.AbstractStatusCode,
-                           StatusDate = h.CreatedDate,
-                           EvaluationID = h.EvaluationId,
-                           KappaType = KappaTypeEnum.K1,
-                           IsParent = true
-                       };
+           
 
-            List<AbstractListRow> abstracts = data.ToList();
+        //    List<AbstractListRow> abstracts = data.ToList();
 
-            //if (AbstractViewGridView.Attributes["CurrentSortExp"] != null)
-            //{
-            //    sort = AbstractViewGridView.Attributes["CurrentSortExp"];
-            //    direction = AbstractViewGridView.Attributes["CurrentSortDir"] == "ASC" ? SortDirection.Ascending : SortDirection.Descending;
-            //}
+        //    //objStopWatch.Stop();
+        //    //System.Diagnostics.Trace.Write("The time taken to execute query without compilation is : " +
+        //    //objStopWatch.ElapsedMilliseconds.ToString() + " MillionSeconds<br>");
+        //    //objStopWatch.Reset();
+           
+        //    //db.Log = Console.Out;
 
-            return AbstractListViewHelper.SortAbstracts(abstracts, sort, direction);
-        }
+        //    //if (AbstractViewGridView.Attributes["CurrentSortExp"] != null)
+        //    //{
+        //    //    sort = AbstractViewGridView.Attributes["CurrentSortExp"];
+        //    //    direction = AbstractViewGridView.Attributes["CurrentSortDir"] == "ASC" ? SortDirection.Ascending : SortDirection.Descending;
+        //    //}
+
+        //    return AbstractListViewHelper.SortAbstracts(abstracts, sort, direction);
+        //}
+        
+        //protected List<AbstractListRow> GetParentAbstractsODPSupervisorOpen(string sort = "", SortDirection direction = SortDirection.Ascending)
+        //{
+        //    string connStr = ConfigurationManager.ConnectionStrings["ODPTaxonomy"].ConnectionString;
+        //    DataJYDataContext db = new DataJYDataContext(connStr);
+
+        //    var data = from a in db.Abstracts
+        //               join h in db.AbstractStatusChangeHistories on a.AbstractID equals h.AbstractID
+        //               join s in db.AbstractStatus on h.AbstractStatusID equals s.AbstractStatusID
+        //               where (
+        //                  (h.AbstractStatusID == (int)AbstractStatusEnum.RETRIEVED_FOR_ODP_CODING_2 ||
+        //                  h.AbstractStatusID == (int)AbstractStatusEnum.CODED_BY_ODP_STAFF_2A) &&
+        //                  h.AbstractStatusChangeHistoryID == db.AbstractStatusChangeHistories
+        //                   .Where(h2 => h2.AbstractID == a.AbstractID)
+        //                   .Select(h2 => h2.AbstractStatusChangeHistoryID).Max()
+        //                   )
+        //               select new AbstractListRow
+        //               {
+        //                   AbstractID = a.AbstractID,
+        //                   ProjectTitle = a.ProjectTitle + " (" + s.AbstractStatusCode + ")",
+        //                   ApplicationID = a.ApplicationID,
+        //                   AbstractStatusID = s.AbstractStatusID,
+        //                   AbstractStatusCode = s.AbstractStatusCode,
+        //                   StatusDate = h.CreatedDate,
+        //                   EvaluationID = h.EvaluationId,
+        //                   KappaType = KappaTypeEnum.K1,
+        //                   IsParent = true
+        //               };
+
+        //    List<AbstractListRow> abstracts = data.ToList();
+
+        //    //if (AbstractViewGridView.Attributes["CurrentSortExp"] != null)
+        //    //{
+        //    //    sort = AbstractViewGridView.Attributes["CurrentSortExp"];
+        //    //    direction = AbstractViewGridView.Attributes["CurrentSortDir"] == "ASC" ? SortDirection.Ascending : SortDirection.Descending;
+        //    //}
+
+        //    return AbstractListViewHelper.SortAbstracts(abstracts, sort, direction);
+        //}
+        //protected List<AbstractListRow> GetParentAbstractsODPSupervisorReviewUnCoded(string sort = "", SortDirection direction = SortDirection.Ascending)
+        //{
+        //    string connStr = ConfigurationManager.ConnectionStrings["ODPTaxonomy"].ConnectionString;
+        //    DataJYDataContext db = new DataJYDataContext(connStr);
+
+        //    var data = from a in db.Abstracts
+        //               join h in db.AbstractStatusChangeHistories on a.AbstractID equals h.AbstractID
+        //               join s in db.AbstractStatus on h.AbstractStatusID equals s.AbstractStatusID
+        //               join rv in db.AbstractReviewLists on a.AbstractID equals rv.AbstractID
+        //               where (
+        //                  h.AbstractStatusID == (int)AbstractStatusEnum.CONSENSUS_COMPLETE_WITH_NOTES_1N &&
+        //                  h.AbstractStatusChangeHistoryID == db.AbstractStatusChangeHistories
+        //                   .Where(h2 => h2.AbstractID == a.AbstractID)
+        //                   .Select(h2 => h2.AbstractStatusChangeHistoryID).Max()
+        //                   )
+        //               select new AbstractListRow
+        //               {
+        //                   AbstractID = a.AbstractID,
+        //                   ProjectTitle = a.ProjectTitle + " (" + s.AbstractStatusCode + ")",
+        //                   ApplicationID = a.ApplicationID,
+        //                   AbstractStatusID = s.AbstractStatusID,
+        //                   AbstractStatusCode = s.AbstractStatusCode,
+        //                   StatusDate = h.CreatedDate,
+        //                   EvaluationID = h.EvaluationId,
+        //                   KappaType = KappaTypeEnum.K1,
+        //                   IsParent = true
+        //               };
+
+        //    List<AbstractListRow> abstracts = data.ToList();
+
+        //    //if (AbstractViewGridView.Attributes["CurrentSortExp"] != null)
+        //    //{
+        //    //    sort = AbstractViewGridView.Attributes["CurrentSortExp"];
+        //    //    direction = AbstractViewGridView.Attributes["CurrentSortDir"] == "ASC" ? SortDirection.Ascending : SortDirection.Descending;
+        //    //}
+
+        //    return AbstractListViewHelper.SortAbstracts(abstracts, sort, direction);
+        //}
 
         protected List<AbstractListRow> GetParentAbstractsODPStaffMemberDefault(string sort = "", SortDirection direction = SortDirection.Ascending)
         {
