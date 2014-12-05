5ef08af3ed43e018eaf9b4faea0adff3c5f69bcc
ttimonina <ttimonina@iqsolutions.com>
2014-12-05 15:15:44 -0500

    SAX parser one sheet

--- SourceCode/ODPTaxonomyUtility_TT/CreateExcelFile.cs
+++ SourceCode/ODPTaxonomyUtility_TT/CreateExcelFile.cs
@@ -201,7 +201,8 @@ namespace ODPTaxonomyUtility_TT
             {
                 using (SpreadsheetDocument document = SpreadsheetDocument.Create(excelFilename, SpreadsheetDocumentType.Workbook))
                 {
-                    WriteExcelFile(ds, document);
+                    //WriteExcelFile(ds, document);
+                    WriteExcelFileSAX(ds, document);
                 }
                 
             }
@@ -210,6 +211,115 @@ namespace ODPTaxonomyUtility_TT
                 throw ex;
             }
         }
+
+        private static void WriteExcelFileSAX(DataSet ds, SpreadsheetDocument myDoc)
+        {
+
+            List<OpenXmlAttribute> xmlAttributes;
+            OpenXmlWriter writer;
+            uint worksheetNumber = 1;
+
+            //add a workbookpart manually
+            myDoc.AddWorkbookPart();
+            WorksheetPart worksheetpart = myDoc.WorkbookPart.AddNewPart<WorksheetPart>();
+
+            //create an XML writer for the worksheetpart
+            writer = OpenXmlWriter.Create(worksheetpart);
+            writer.WriteStartElement(new Worksheet());
+            writer.WriteStartElement(new SheetData());
+            
+
+            
+            foreach (DataTable dt in ds.Tables)
+            {
+
+            
+
+
+            //DataTable dt = ds.Tables[0];            
+
+            int rowNumber = 1;
+            int numberOfColumns = dt.Columns.Count;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                
+
+                xmlAttributes = new List<OpenXmlAttribute>();
+                // this is the row index
+                xmlAttributes.Add(new OpenXmlAttribute("r", null, rowNumber.ToString()));
+
+                //write the row start element with the attributes added above
+                writer.WriteStartElement(new Row(), xmlAttributes);
+
+                for (int columnNumber = 0; columnNumber < numberOfColumns; columnNumber++)
+                {
+                    string cellValue = dr.ItemArray[columnNumber].ToString();
+
+                    //reset the attributes
+                    xmlAttributes = new List<OpenXmlAttribute>();
+                    // this is the data type ("t"), with CellValues.String ("str")
+                    // you might need to change this depending on your source data
+                    // you might also consider using the Shared Strings table instead
+                    xmlAttributes.Add(new OpenXmlAttribute("t", null, "str"));
+
+                    //add the cell reference (A1, B1, A2... etc)
+                    xmlAttributes.Add(new OpenXmlAttribute("r", null, "column"));
+
+                    //write the start of the cell element with the type and cell reference attributes
+                    writer.WriteStartElement(new Cell(), xmlAttributes);
+
+                    //write the cell value
+                    writer.WriteElement(new CellValue(cellValue));
+
+                    //write the cell end element
+                    writer.WriteEndElement();
+                }
+
+                //write the row end element
+                writer.WriteEndElement();
+
+                rowNumber++;
+            }
+
+            //write the sheetdata end element
+            writer.WriteEndElement();
+            //write the worksheet end element
+            writer.WriteEndElement(); 
+            writer.Close();
+
+               
+
+
+
+            //create a writer for the workbookpart
+            writer = OpenXmlWriter.Create(myDoc.WorkbookPart);
+            //write the start element of a workbook to the workbook part
+            writer.WriteStartElement(new Workbook());
+            //write the start element of a sheets item to the workbook part
+            writer.WriteStartElement(new Sheets());
+            //write the whole element of a sheet to the workbook part
+            //note we link it to the id of the worksheetpart populated above
+            writer.WriteElement(new Sheet()
+            {
+                Name = dt.TableName,
+                SheetId = worksheetNumber,
+                Id = myDoc.WorkbookPart.GetIdOfPart(worksheetpart)
+            });
+
+
+            }//foreach
+
+
+            //write the sheets end element
+            writer.WriteEndElement();
+            //write the workbook end element
+            writer.WriteEndElement();
+            writer.Close();
+
+
+        }
+
 
         private static void WriteExcelFile(DataSet ds, SpreadsheetDocument spreadsheet)
         {
