6b3e16d232ab7f15c9d20127d130db53090f6e93
Sunil Taneja <suniltaneja@me.com>
2016-04-14 16:54:44 -0400

    customizing the scroll offset.

--- SourceCode/ODPTaxonomyWebsite/Scripts/main.js
+++ SourceCode/ODPTaxonomyWebsite/Scripts/main.js
@@ -75,20 +75,24 @@ $(document).ready(function(){
 
 //Scroll for anchors
 
-    //$(function () {
-    //$('a[href*=#]:not([href=#])').click(function () {
-    //    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    //        var target = $(this.hash);
-    //        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    //        if (target.length) {
-    //            $('html,body').animate({
-    //scrollTop: target.offset().top
-    //}, 1000);
-    //            return false;
-    //}
-    //}
-    //});
-    //});
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if(this.hash == '#IQS' || this.hash == '#ODP'){
+                //console.log(this.hash);
+                return;
+            }
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                        scrollTop: target.offset().top
+                        }, 1000);
+                return false;
+    }
+    }
+    });
+    });
 
 
 
