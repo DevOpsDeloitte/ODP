9cc7080ed7663a8260b54615ac831341a38a7f1a
Sunil Taneja <suniltaneja@me.com>
2016-06-01 15:18:03 -0400

    refactoring some queries.

--- SourceCode/ODPTaxonomyDAL_JY/AbstractListViewData.cs
+++ SourceCode/ODPTaxonomyDAL_JY/AbstractListViewData.cs
@@ -39,15 +39,15 @@ namespace ODPTaxonomyDAL_JY
         public IEnumerable<KappaData> getAllKappaRecordsK1K2Only(List<int> AbstractIDs)
         {
             var query = from k in db2.KappaDatas
-                        where (k.KappaTypeID == 1 || k.KappaTypeID == 2) && AbstractIDs.Contains(k.AbstractID ?? 0)
+                        where k.KappaTypeID == 1 || k.KappaTypeID == 2
                         select k;
-            return query.ToList<KappaData>();
+            return query.ToList<KappaData>().Where(q => AbstractIDs.Contains(q.AbstractID ?? 0));
         }
 
         public IEnumerable<Submission> getAllSubmissionRecords(List<int> EvaluationIDs)
         {
             var query = from k in db2.Submissions
-                        where EvaluationIDs.Contains(k.EvaluationId ?? 0)
+                        //where EvaluationIDs.Contains(k.EvaluationId ?? 0)
                         select k;
             return query.ToList<Submission>();
         }
@@ -55,7 +55,7 @@ namespace ODPTaxonomyDAL_JY
         public IEnumerable<Evaluation> getAllEvaluationRecords(List<int> AbstractIDs)
         {
             var query = from k in db2.Evaluations
-                        where AbstractIDs.Contains(k.AbstractID ?? 0)
+                        //where AbstractIDs.Contains(k.AbstractID ?? 0)
                         select k;
             return query.ToList<Evaluation>();
         }
@@ -71,7 +71,7 @@ namespace ODPTaxonomyDAL_JY
         public IEnumerable<F_PreventionCategoryAnswer> getAllF_PreventionCategoryRecordsID6(List<int> SubmissionIDs)
         {
             var query = from k in db2.F_PreventionCategoryAnswers
-                        where k.PreventionCategoryID == 6 && SubmissionIDs.Contains(k.SubmissionID)
+                        where k.PreventionCategoryID == 6 // && SubmissionIDs.Contains(k.SubmissionID)
                         select k;
             return query.ToList<F_PreventionCategoryAnswer>();
         }
@@ -95,7 +95,7 @@ namespace ODPTaxonomyDAL_JY
         public IEnumerable<E_StudyDesignPurposeAnswer> getAllE_StudyDesignPurposeRecordsID7(IEnumerable<int> SubmissionIDs)
         {
             var query = from k in db2.E_StudyDesignPurposeAnswers
-                        where k.StudyDesignPurposeID == 7 && SubmissionIDs.Contains(k.SubmissionID)
+                        where k.StudyDesignPurposeID == 7 // && SubmissionIDs.Contains(k.SubmissionID)
                         select k;
             return query.ToList<E_StudyDesignPurposeAnswer>();
         }
@@ -111,7 +111,7 @@ namespace ODPTaxonomyDAL_JY
         public IEnumerable<E_StudyDesignPurposeAnswer> getAllE_StudyDesignPurposeRecords(IEnumerable<int> SubmissionIds)
         {
             var query = from k in db2.E_StudyDesignPurposeAnswers
-                        where SubmissionIds.Contains(k.SubmissionID)
+                        //where SubmissionIds.Contains(k.SubmissionID)
                         select k;
             return query.ToList<E_StudyDesignPurposeAnswer>();
         }
@@ -206,7 +206,7 @@ namespace ODPTaxonomyDAL_JY
         }
        
 
-        public IEnumerable<F_PreventionCategoryAnswer> getAllF_PreventionCategoryRecords(IEnumerable<int> SubmissionIds)
+        public IEnumerable<F_PreventionCategoryAnswer> getAllF_PreventionCategoryRecords(List<int> SubmissionIds)
         {
             var query = from k in db2.F_PreventionCategoryAnswers
                         where SubmissionIds.Contains(k.SubmissionID)
