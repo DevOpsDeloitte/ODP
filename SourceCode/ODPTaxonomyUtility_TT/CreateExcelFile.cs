2fcb08ba8b632addf2a89a25added55758c99ec2
Sunil Taneja <suniltaneja@me.com>
2017-01-09 11:26:54 -0500

    Revert "updates for header"
    This reverts commit 69ff603e6a598d844990e7f72742d91cc8c7de97.

--- SourceCode/ODPTaxonomyUtility_TT/CreateExcelFile.cs
+++ SourceCode/ODPTaxonomyUtility_TT/CreateExcelFile.cs
@@ -496,117 +496,6 @@ namespace ODPTaxonomyUtility_TT
                 IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32");
             }
 
-            var headerRow2 = new Row { RowIndex = ++rowIndex };  // add a row at the top of spreadsheet
-            sheetData.Append(headerRow2);
-
-            for (int colInx = 0; colInx < numberOfColumns; colInx++)
-            {
-                DataColumn col = dt.Columns[colInx];
-                AppendTextCell(excelColumnNames[colInx] + "2", col.ColumnName, headerRow2, true);
-                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32");
-            }
-
-            var headerRow3 = new Row { RowIndex = ++rowIndex };  // add a row at the top of spreadsheet
-            sheetData.Append(headerRow3);
-
-            for (int colInx = 0; colInx < numberOfColumns; colInx++)
-            {
-                DataColumn col = dt.Columns[colInx];
-                AppendTextCell(excelColumnNames[colInx] + "3", col.ColumnName, headerRow3, true);
-                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32");
-            }
-
-
-            //
-            //  Now, step through each row of data in our DataTable...
-            //
-            double cellNumericValue = 0;
-            foreach (DataRow dr in dt.Rows)
-            {
-                // ...create a new row, and append a set of this row's data to it.
-                ++rowIndex;
-                var newExcelRow = new Row { RowIndex = rowIndex };  // add a row at the top of spreadsheet
-                sheetData.Append(newExcelRow);
-
-                for (int colInx = 0; colInx < numberOfColumns; colInx++)
-                {
-                    cellValue = dr.ItemArray[colInx].ToString();
-
-                    // Create cell with data
-                    if (IsNumericColumn[colInx])
-                    {
-                        //  For numeric cells, make sure our input data IS a number, then write it out to the Excel file.
-                        //  If this numeric value is NULL, then don't write anything to the Excel file.
-                        cellNumericValue = 0;
-                        if (double.TryParse(cellValue, out cellNumericValue))
-                        {
-                            cellValue = cellNumericValue.ToString();
-                            AppendNumericCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, newExcelRow);
-                        }
-                    }
-                    else
-                    {
-                        //  For text cells, just write the input data straight out to the Excel file.
-                        AppendTextCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, newExcelRow, false);
-                    }
-                }
-            }
-        }
-
-        private static void WriteDataTableToExcelWorksheet_KappaHeaders(DataTable dt, WorksheetPart worksheetPart)
-        {
-            var worksheet = worksheetPart.Worksheet;
-            var sheetData = worksheet.GetFirstChild<SheetData>();
-
-            string cellValue = "";
-
-            //  Create a Header Row in our Excel file, containing one header for each Column of data in our DataTable.
-            //
-            //  We'll also create an array, showing which type each column of data is (Text or Numeric), so when we come to write the actual
-            //  cells of data, we'll know if to write Text values or Numeric cell values.
-            int numberOfColumns = dt.Columns.Count;
-            bool[] IsNumericColumn = new bool[numberOfColumns];
-
-            string[] excelColumnNames = new string[numberOfColumns];
-            for (int n = 0; n < numberOfColumns; n++)
-                excelColumnNames[n] = GetExcelColumnName(n);
-
-            //
-            //  Create the Header row in our Excel Worksheet
-            //
-            uint rowIndex = 1;
-
-            var headerRow = new Row { RowIndex = rowIndex };  // add a row at the top of spreadsheet
-            sheetData.Append(headerRow);
-
-            for (int colInx = 0; colInx < numberOfColumns; colInx++)
-            {
-                DataColumn col = dt.Columns[colInx];
-                AppendTextCell(excelColumnNames[colInx] + "1", col.ColumnName, headerRow, true);
-                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32");
-            }
-
-            var headerRow2 = new Row { RowIndex = ++rowIndex };  // add a row at the top of spreadsheet
-            sheetData.Append(headerRow2);
-
-            for (int colInx = 0; colInx < numberOfColumns; colInx++)
-            {
-                DataColumn col = dt.Columns[colInx];
-                AppendTextCell(excelColumnNames[colInx] + "2", col.ColumnName, headerRow2, true);
-                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32");
-            }
-
-            var headerRow3 = new Row { RowIndex = ++rowIndex };  // add a row at the top of spreadsheet
-            sheetData.Append(headerRow3);
-
-            for (int colInx = 0; colInx < numberOfColumns; colInx++)
-            {
-                DataColumn col = dt.Columns[colInx];
-                AppendTextCell(excelColumnNames[colInx] + "3", col.ColumnName, headerRow3, true);
-                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32");
-            }
-
-
             //
             //  Now, step through each row of data in our DataTable...
             //
@@ -656,19 +545,6 @@ namespace ODPTaxonomyUtility_TT
                 cell = new Cell() { CellReference = cellReference, DataType = CellValues.String };
             }
             
-            CellValue cellValue = new CellValue();
-            cellValue.Text = cellStringValue;
-            cell.Append(cellValue);
-            excelRow.Append(cell);
-        }
-
-        private static void AppendTextCellYellow(string cellReference, string cellStringValue, Row excelRow)
-        {
-            //  Add a new Excel Cell to our Row 
-            Cell cell = null;
-
-            cell = new Cell() { CellReference = cellReference, DataType = CellValues.String, StyleIndex = 6 };
-        
             CellValue cellValue = new CellValue();
             cellValue.Text = cellStringValue;
             cell.Append(cellValue);
