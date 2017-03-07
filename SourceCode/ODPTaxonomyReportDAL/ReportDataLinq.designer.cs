270cbe380bedd47ad78282e18e2878f663fb4195
jazchan <jchan@iqsolutions.com>
2017-03-07 09:40:33 -0500

    add new project number to report

--- SourceCode/ODPTaxonomyReportDAL/ReportDataLinq.designer.cs
+++ SourceCode/ODPTaxonomyReportDAL/ReportDataLinq.designer.cs
@@ -62,134 +62,18 @@ namespace ODPTaxonomyReportDAL
 			OnCreated();
 		}
 		
-		[global::System.Data.Linq.Mapping.FunctionAttribute(Name="dbo.rpt_IQCode_Abstracts_ByDate")]
-		public ISingleResult<rpt_IQCode_Abstracts_ByDateResult> rpt_IQCode_Abstracts_ByDate([global::System.Data.Linq.Mapping.ParameterAttribute(Name="DateStart", DbType="VarChar(100)")] string dateStart, [global::System.Data.Linq.Mapping.ParameterAttribute(Name="DateEnd", DbType="VarChar(100)")] string dateEnd)
-		{
-			IExecuteResult result = this.ExecuteMethodCall(this, ((MethodInfo)(MethodInfo.GetCurrentMethod())), dateStart, dateEnd);
-			return ((ISingleResult<rpt_IQCode_Abstracts_ByDateResult>)(result.ReturnValue));
-		}
-		
 		[global::System.Data.Linq.Mapping.FunctionAttribute(Name="dbo.rpt_IQCode_Abstracts_Consensus_ByDate")]
 		public ISingleResult<rpt_IQCode_Abstracts_Consensus_ByDateResult> rpt_IQCode_Abstracts_Consensus_ByDate([global::System.Data.Linq.Mapping.ParameterAttribute(Name="DateStart", DbType="VarChar(100)")] string dateStart, [global::System.Data.Linq.Mapping.ParameterAttribute(Name="DateEnd", DbType="VarChar(100)")] string dateEnd)
 		{
 			IExecuteResult result = this.ExecuteMethodCall(this, ((MethodInfo)(MethodInfo.GetCurrentMethod())), dateStart, dateEnd);
 			return ((ISingleResult<rpt_IQCode_Abstracts_Consensus_ByDateResult>)(result.ReturnValue));
 		}
-	}
-	
-	public partial class rpt_IQCode_Abstracts_ByDateResult
-	{
 		
-		private string _Date;
-		
-		private string _GroupUsers;
-		
-		private string _Flagged_Y_N;
-		
-		private System.Nullable<int> _Appl_ID;
-		
-		private string _PI_Name;
-		
-		private string _Title;
-		
-		public rpt_IQCode_Abstracts_ByDateResult()
+		[global::System.Data.Linq.Mapping.FunctionAttribute(Name="dbo.rpt_IQCode_Abstracts_ByDate")]
+		public ISingleResult<rpt_IQCode_Abstracts_ByDateResult> rpt_IQCode_Abstracts_ByDate([global::System.Data.Linq.Mapping.ParameterAttribute(Name="DateStart", DbType="VarChar(100)")] string dateStart, [global::System.Data.Linq.Mapping.ParameterAttribute(Name="DateEnd", DbType="VarChar(100)")] string dateEnd)
 		{
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Date", DbType="VarChar(30)")]
-		public string Date
-		{
-			get
-			{
-				return this._Date;
-			}
-			set
-			{
-				if ((this._Date != value))
-				{
-					this._Date = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_GroupUsers", DbType="VarChar(500)")]
-		public string GroupUsers
-		{
-			get
-			{
-				return this._GroupUsers;
-			}
-			set
-			{
-				if ((this._GroupUsers != value))
-				{
-					this._GroupUsers = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[Flagged Y/N]", Storage="_Flagged_Y_N", DbType="VarChar(1) NOT NULL", CanBeNull=false)]
-		public string Flagged_Y_N
-		{
-			get
-			{
-				return this._Flagged_Y_N;
-			}
-			set
-			{
-				if ((this._Flagged_Y_N != value))
-				{
-					this._Flagged_Y_N = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[Appl ID]", Storage="_Appl_ID", DbType="Int")]
-		public System.Nullable<int> Appl_ID
-		{
-			get
-			{
-				return this._Appl_ID;
-			}
-			set
-			{
-				if ((this._Appl_ID != value))
-				{
-					this._Appl_ID = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[PI Name]", Storage="_PI_Name", DbType="NVarChar(255)")]
-		public string PI_Name
-		{
-			get
-			{
-				return this._PI_Name;
-			}
-			set
-			{
-				if ((this._PI_Name != value))
-				{
-					this._PI_Name = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Title", DbType="NVarChar(255) NOT NULL", CanBeNull=false)]
-		public string Title
-		{
-			get
-			{
-				return this._Title;
-			}
-			set
-			{
-				if ((this._Title != value))
-				{
-					this._Title = value;
-				}
-			}
+			IExecuteResult result = this.ExecuteMethodCall(this, ((MethodInfo)(MethodInfo.GetCurrentMethod())), dateStart, dateEnd);
+			return ((ISingleResult<rpt_IQCode_Abstracts_ByDateResult>)(result.ReturnValue));
 		}
 	}
 	
@@ -200,7 +84,11 @@ namespace ODPTaxonomyReportDAL
 		
 		private string _GroupUsers;
 		
-		private int _Appl_ID;
+		private string _Flagged_Y_N;
+		
+		private string _ProjectNumber;
+		
+		private System.Nullable<int> _Appl_ID;
 		
 		private string _PI_Name;
 		
@@ -258,8 +146,40 @@ namespace ODPTaxonomyReportDAL
 			}
 		}
 		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[Appl ID]", Storage="_Appl_ID", DbType="Int NOT NULL")]
-		public int Appl_ID
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[Flagged Y/N]", Storage="_Flagged_Y_N", DbType="VarChar(1) NOT NULL", CanBeNull=false)]
+		public string Flagged_Y_N
+		{
+			get
+			{
+				return this._Flagged_Y_N;
+			}
+			set
+			{
+				if ((this._Flagged_Y_N != value))
+				{
+					this._Flagged_Y_N = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_ProjectNumber", DbType="NVarChar(255)")]
+		public string ProjectNumber
+		{
+			get
+			{
+				return this._ProjectNumber;
+			}
+			set
+			{
+				if ((this._ProjectNumber != value))
+				{
+					this._ProjectNumber = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[Appl ID]", Storage="_Appl_ID", DbType="Int")]
+		public System.Nullable<int> Appl_ID
 		{
 			get
 			{
@@ -430,6 +350,140 @@ namespace ODPTaxonomyReportDAL
 				if ((this._F != value))
 				{
 					this._F = value;
+				}
+			}
+		}
+	}
+	
+	public partial class rpt_IQCode_Abstracts_ByDateResult
+	{
+		
+		private string _Date;
+		
+		private string _GroupUsers;
+		
+		private string _Flagged_Y_N;
+		
+		private string _ProjectNumber;
+		
+		private System.Nullable<int> _Appl_ID;
+		
+		private string _PI_Name;
+		
+		private string _Title;
+		
+		public rpt_IQCode_Abstracts_ByDateResult()
+		{
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Date", DbType="VarChar(30)")]
+		public string Date
+		{
+			get
+			{
+				return this._Date;
+			}
+			set
+			{
+				if ((this._Date != value))
+				{
+					this._Date = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_GroupUsers", DbType="VarChar(500)")]
+		public string GroupUsers
+		{
+			get
+			{
+				return this._GroupUsers;
+			}
+			set
+			{
+				if ((this._GroupUsers != value))
+				{
+					this._GroupUsers = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[Flagged Y/N]", Storage="_Flagged_Y_N", DbType="VarChar(1) NOT NULL", CanBeNull=false)]
+		public string Flagged_Y_N
+		{
+			get
+			{
+				return this._Flagged_Y_N;
+			}
+			set
+			{
+				if ((this._Flagged_Y_N != value))
+				{
+					this._Flagged_Y_N = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_ProjectNumber", DbType="NVarChar(255)")]
+		public string ProjectNumber
+		{
+			get
+			{
+				return this._ProjectNumber;
+			}
+			set
+			{
+				if ((this._ProjectNumber != value))
+				{
+					this._ProjectNumber = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[Appl ID]", Storage="_Appl_ID", DbType="Int")]
+		public System.Nullable<int> Appl_ID
+		{
+			get
+			{
+				return this._Appl_ID;
+			}
+			set
+			{
+				if ((this._Appl_ID != value))
+				{
+					this._Appl_ID = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Name="[PI Name]", Storage="_PI_Name", DbType="NVarChar(255)")]
+		public string PI_Name
+		{
+			get
+			{
+				return this._PI_Name;
+			}
+			set
+			{
+				if ((this._PI_Name != value))
+				{
+					this._PI_Name = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Title", DbType="NVarChar(255) NOT NULL", CanBeNull=false)]
+		public string Title
+		{
+			get
+			{
+				return this._Title;
+			}
+			set
+			{
+				if ((this._Title != value))
+				{
+					this._Title = value;
 				}
 			}
 		}
