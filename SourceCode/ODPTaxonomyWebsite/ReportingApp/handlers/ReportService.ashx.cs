209a46a83f4a07d0b69104e892303a6f470f981a
Sunil Taneja <suniltaneja@me.com>
2017-03-28 10:12:53 -0400

    mapping new stored proc

--- SourceCode/ODPTaxonomyWebsite/ReportingApp/handlers/ReportService.ashx.cs
+++ SourceCode/ODPTaxonomyWebsite/ReportingApp/handlers/ReportService.ashx.cs
@@ -69,6 +69,7 @@ namespace ODPTaxonomyWebsite.ReportingApp.handlers
                 string ktype = context.Request["ktype"] ?? "";
                 string mechanisms = context.Request["mechanisms"] ?? "";
                 string[] allmechanisms = mechanisms.Split(',');
+                List<string> selectedMechanisms = new List<string>();
                 DataSet ds = new DataSet();
                 int mechCount = allmechanisms.Length;
                 List<List<Report_KappaAvg_ByQCWeeksResult>> reports = new List<List<Report_KappaAvg_ByQCWeeksResult>>();
@@ -77,18 +78,21 @@ namespace ODPTaxonomyWebsite.ReportingApp.handlers
                     var m = mechanism.Split('-');
                     var mechanism_id = m[0];
                     var mechanism_name = m[1];
+                    selectedMechanisms.Add(mechanism_id);
                     List<Report_KappaAvg_ByQCWeeksResult> reportvals = db.Report_KappaAvg_ByQCWeeks(start, end, ktype).ToList();
                     CreateExcelFile.CreateExcelDocumentPrecision<Report_KappaAvg_ByQCWeeksResult>(reportvals, context.Response, mechanism_name +"-"+ ktype, ds);
 
                 }
 
                // List<Report_KappaAvg_ByQCWeeksResult> reportvals = db.Report_KappaAvg_ByQCWeeks(start, end, ktype).ToList();
-                List<Report_KappaAvg_DataDetail_ByQCWeeksResult> reportvalsdetail = db.Report_KappaAvg_DataDetail_ByQCWeeks(start, end, ktype).ToList();               
+                List<Report_KappaAvg_DataDetail_ByQCWeeks_NewResult> reportvalsdetail = db.Report_KappaAvg_DataDetail_ByQCWeeks_New(start, end, ktype, String.Join(",",selectedMechanisms.ToArray())).ToList();
+                CreateExcelFile.CreateExcelDocumentPrecision<Report_KappaAvg_DataDetail_ByQCWeeks_NewResult>(reportvalsdetail, context.Response, "KappaAvgDetail-" + ktype, ds);
+                CreateExcelFile.CreateExcelDocumentAsStreamSpecialHeaders(ds, "KappaAvg-" + start + "-" + end + "-" + ktype + ".xlsx", context.Response, start, end);
                 //CreateExcelFile.CreateExcelDocumentPrecision<Report_KappaAvg_ByQCWeeksResult>(reportvals, context.Response, "KappaAvg-"+ktype, ds);
                 //CreateExcelFile.CreateExcelDocumentPrecision<Report_KappaAvg_ByQCWeeksResult>(reportvals, context.Response, "KappaAvg-2" + ktype, ds);
                 //CreateExcelFile.CreateExcelDocumentPrecision<Report_KappaAvg_ByQCWeeksResult>(reportvals, context.Response, "KappaAvg-3" + ktype, ds);
-                CreateExcelFile.CreateExcelDocumentPrecision<Report_KappaAvg_DataDetail_ByQCWeeksResult>(reportvalsdetail, context.Response, "KappaAvgDetail-" + ktype, ds);
-                CreateExcelFile.CreateExcelDocumentAsStreamSpecialHeaders(ds, "KappaAvg-"+start+"-"+end+"-"+ktype+".xlsx", context.Response, start, end);
+                //CreateExcelFile.CreateExcelDocumentPrecision<Report_KappaAvg_DataDetail_ByQCWeeksResult>(reportvalsdetail, context.Response, "KappaAvgDetail-" + ktype, ds);
+                //CreateExcelFile.CreateExcelDocumentAsStreamSpecialHeaders(ds, "KappaAvg-"+start+"-"+end+"-"+ktype+".xlsx", context.Response, start, end);
                 //return JsonConvert.SerializeObject(reportvals);
             }
         }
